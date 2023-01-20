01ce041958a7ceacb37820a5da6b41ee0218911a
Geoff Schmit <gcschmit@gmail.com>
2023-01-20 14:04:13 -0600

    remove confusing line wrap

--- Picture.java
+++ Picture.java
@@ -135,8 +135,7 @@ public class Picture extends SimplePicture
             {
 
                 leftPixel = pixels[row][col];      
-                rightPixel = pixels[row]                       
-                [mirrorPoint - col + mirrorPoint];
+                rightPixel = pixels[row][mirrorPoint - col + mirrorPoint];
                 rightPixel.setColor(leftPixel.getColor());
             }
         }
